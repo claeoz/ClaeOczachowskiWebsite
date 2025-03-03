ecaf375cd33123f08155853b3e01c3c4acfe437e
claeoz <126366391+claeoz@users.noreply.github.com>
2025-03-03 15:23:42 -0600

    applied html (did not tailor to angular)

--- ClaeOczachowskiWebsite/src/app/app.component.ts
+++ ClaeOczachowskiWebsite/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, [NavBarComponent]],
+  imports: [RouterOutlet, [NavBarComponent], [HomeComponent]],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
