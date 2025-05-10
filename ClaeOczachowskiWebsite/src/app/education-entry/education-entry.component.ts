6da855dbe8321cc38bcaddcd1f2195fcc2d38a4e
claeoz <126366391+claeoz@users.noreply.github.com>
2025-05-10 17:24:54 -0500

    education implement input

--- ClaeOczachowskiWebsite/src/app/education-entry/education-entry.component.ts
+++ ClaeOczachowskiWebsite/src/app/education-entry/education-entry.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 
 @Component({
   selector: 'education-entry',
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './education-entry.component.css'
 })
 export class EducationEntryComponent {
-
+  @Input() start: string = "couldn't read start";
+  @Input() finish: string = "Present";
+  @Input() place: string = "couldn't read place"
+  @Input() details: string = "couldn't read details"
 }
