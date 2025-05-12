7b986bac03e1c7fbff5959175dc44cd40ada633a
claeoz <126366391+claeoz@users.noreply.github.com>
2025-05-12 06:37:46 -0500

    started job entry component

---
+++ ClaeOczachowskiWebsite/src/app/job-entry/job-entry.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'job-entry',
+  imports: [],
+  templateUrl: './job-entry.component.html',
+  styleUrl: './job-entry.component.css'
+})
+export class JobEntryComponent {
+
+}
--- ClaeOczachowskiWebsite/src/app/resume/resume.component.ts
+++ ClaeOczachowskiWebsite/src/app/resume/resume.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { ResumeEntryComponent } from "../resume-entry/resume-entry.component";
+import { JobEntryComponent } from '../job-entry/job-entry.component';
 @Component({
   selector: 'app-resume',
-  imports: [ResumeEntryComponent],
+  imports: [ResumeEntryComponent, JobEntryComponent],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
