da7e8b02200c51f092f571b604c285d0b8d8ac72
claeoz <126366391+claeoz@users.noreply.github.com>
2025-05-22 02:02:28 -0500

    resume skills now utilizes a list

--- ClaeOczachowskiWebsite/src/app/resume/resume.component.ts
+++ ClaeOczachowskiWebsite/src/app/resume/resume.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { ResumeEntryComponent } from "../resume-entry/resume-entry.component";
 import { JobEntryComponent } from '../job-entry/job-entry.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-resume',
-  imports: [ResumeEntryComponent, JobEntryComponent],
+  imports: [ResumeEntryComponent, JobEntryComponent, CommonModule],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
 export class ResumeComponent {
-
+ skills: string[] = [
+  "C#",
+  "C",
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "typeScript",
+  "Bootstrap",
+  "JQuery",
+  "Angular",
+  "GDScript",
+  "Bash",
+  "PowerShell",
+  "Git",
+  "Github",
+  "Godot",
+  "Blendr",
+  "Agile"
+ ]
 }
