780036046b7a4ceefe8692c70ad3879a4b662a1a
claeoz <126366391+claeoz@users.noreply.github.com>
2025-05-23 21:24:11 -0500

    generated achievement card

---
+++ ClaeOczachowskiWebsite/src/app/achievement-card/achievement-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-achievement-card',
+  imports: [],
+  templateUrl: './achievement-card.component.html',
+  styleUrl: './achievement-card.component.css'
+})
+export class AchievementCardComponent {
+
+}
